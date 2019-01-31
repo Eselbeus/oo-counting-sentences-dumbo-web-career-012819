153815004ec91351743923d309190dad4e2ce7ce
eselbeus <eselbeus@gmail.com>
2019-01-31 14:55:34 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else
+      return false 
+    end  
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      return false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true 
+    else 
+      return false 
+    end
   end
 
   def count_sentences
-
+    count = 0 
+    self.each_char.with_index do |char, i|
+      if char == "." &&  self[i - 1].match(/\w/)
+        count += 1 
+      elsif char == "!" && self[i - 1].match(/\w/)
+        count += 1 
+      elsif char == "?"
+        count += 1 
+      end
+    end
+    count
   end
 end
